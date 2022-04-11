a6c1ffc4928e4b1f151c375473eb87687922b2fb
Sancara <santiagocaraballo@gmail.com>
2022-04-11 00:30:54 -0300

    change the keyboard return to go

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -13,12 +13,16 @@ class WeatherViewController: UIViewController {
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
-    
+    @IBOutlet weak var searchTextField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func searchPress(_ sender: UIButton) {
+        searchTextField.text
+    }
+    
 }
 
